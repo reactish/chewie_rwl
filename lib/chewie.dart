eba7ec03c37df056a854741f00f9ccfb0976e86d
Israel Madueme <im@reactish.com>
2025-04-13 16:35:55 -0400

    progress

--- lib/chewie.dart
+++ lib/chewie.dart
@@ -3,4 +3,5 @@ library;
 export 'src/chewie_player.dart';
 export 'src/chewie_progress_colors.dart';
 export 'src/material/material_progress_bar.dart';
+export 'src/omni_video_controller.dart';
 export 'src/models/index.dart';
